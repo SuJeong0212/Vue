ee94a3653a641477cc2d3be34432233638ca54ac
SuJeong0212 <s.rystal0212@gmail.com>
2022-02-07 01:11:18 +0900

    🚧 data(), mehtods() 적용

--- property/src/App.vue
+++ property/src/App.vue
@@ -1,15 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div class="menu">
+    <a v-for="menu in menus" :key="menu" href="#none">{{menu}}</a>
+  </div>
+
+  <div>
+    <img src="">
+    <h4>{{products[0]}}</h4>
+    <p>100만원</p>
+    <button @click="report[0]++">허위매물신고</button> <span>신고수 : {{report[0]}}</span>
+  </div>
+  <div>
+    <h4>{{products[1]}}</h4>
+    <p>70만원</p>
+    <button @click="report[1]++">허위매물신고</button> <span>신고수 : {{report[1]}}</span>
+  </div>
+  <div>
+    <h4>{{products[2]}}</h4>
+    <p>80만원</p>
+    <button @click="report[2]++">허위매물신고</button> <span>신고수 : {{report[2]}}</span>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data(){
+    return {
+      report : [0,0,0],
+      menus : ['Home', 'Products', 'About'],
+      products : ['역삼동원룸','천호동원룸','마포구원룸']
+    }
+  },
+  methods :{
+    increase(){
+      this.report += 1
+    }
+  },
+
   components: {
-    HelloWorld
   }
 }
 </script>
@@ -21,6 +51,15 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.menu{
+  background:darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a{
+  color:#fff;
+  padding:10px;
 }
 </style>
