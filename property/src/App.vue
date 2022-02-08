85a2ecbfcbe73c1fc258f9505f067daf69be7b7b
SuJeong0212 <s.rystal0212@gmail.com>
2022-02-08 16:26:43 +0900

    🚧 Import , Component, Props

--- property/src/App.vue
+++ property/src/App.vue
@@ -1,33 +1,29 @@
 <template>
+  <Modal :onerooms="onerooms" :click="click" :modalOpen="modalOpen"/>
 
   <div class="menu">
     <a v-for="menu in menus" :key="menu" href="#none">{{menu}}</a>
   </div>
 
-  <div>
-    <img src="">
-    <h4>{{products[0]}}</h4>
-    <p>100만원</p>
-    <button @click="report[0]++">허위매물신고</button> <span>신고수 : {{report[0]}}</span>
-  </div>
-  <div>
-    <h4>{{products[1]}}</h4>
-    <p>70만원</p>
-    <button @click="report[1]++">허위매물신고</button> <span>신고수 : {{report[1]}}</span>
-  </div>
-  <div>
-    <h4>{{products[2]}}</h4>
-    <p>80만원</p>
-    <button @click="report[2]++">허위매물신고</button> <span>신고수 : {{report[2]}}</span>
-  </div>
+  <Discount/>
+
+
+ 
 </template>
 
 <script>
+
+import data from './assets/oneroom.js'
+import Discount from './Discount.vue'
+import Modal from './Modal.vue'
 
 export default {
   name: 'App',
   data(){
     return {
+      click : 0,
+      onerooms : data,
+      modalOpen : false,
       report : [0,0,0],
       menus : ['Home', 'Products', 'About'],
       products : ['역삼동원룸','천호동원룸','마포구원룸']
@@ -40,11 +36,35 @@ export default {
   },
 
   components: {
+    Discount,
+    Modal,
   }
 }
 </script>
 
 <style>
+body{
+  margin:0;
+}
+div{
+  box-sizing: border-box;
+}
+.black-bg{
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0,0,0,0.5);
+  position: fixed;
+  padding:20px;
+}
+.black-bg img{
+  width:100%;
+}
+.white-bg{
+  width: 100%;
+  background-color: #fff;
+  border-radius: 8px;
+  padding: 20px;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -61,5 +81,16 @@ export default {
 .menu a{
   color:#fff;
   padding:10px;
+}
+
+.room-img{
+  width:100%;
+  margin-top:40px;
+}
+.discount{
+  background-color: #eee;
+  padding: 10px;
+  margin: 10px;
+  border-radius: 5px;
 }
 </style>
---
+++ property/src/Modal.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="black-bg" v-if="modalOpen == true">
+    <div class="white-bg">
+      <img :src="onerooms[click].image" alt="">
+      <h4>{{onerooms[click].title}}</h4>
+      <p>{{onerooms[click].content}}</p>
+      <p>{{onerooms[click].price}}</p>
+      <!-- <button @click="modalOpen = false">닫기</button> -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Modal',
+    props:{
+        onerooms : Array,
+        click : Number,
+        modalOpen : Boolean,
+    }
+}
+</script>
+
+<style>
+
+</style>
